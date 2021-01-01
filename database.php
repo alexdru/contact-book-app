746e50039baecb563f0a5c9bc4b0be359236a730
alexdru <alexdru@outlook.com>
2021-01-01 21:37:46 +0500

    hotfix

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $pdo = new PDO('sqlite:contacts.db');
-
-    $statement = $pdo->query("SELECT * FROM contacts");
-
-    $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-    print_r($rows);
