39ade7ad8559bcfb6d5aa33a8117c26f764ecc53
Alex Druzhinin <alexdru@outlook.com>
2021-02-17 20:19:58 +0500

    dockerize laravel

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -3,22 +3,21 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 
 class Cors
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  $request
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-Token-Auth');
     }
 }
